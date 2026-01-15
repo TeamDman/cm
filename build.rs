6163edcb9745dc5f813050e681acafa0cc7028ca
TeamDman <TeamDman9201@gmail.com>
2026-01-14 23:11:22 -0500

    add icon and package attributes in cargo toml

--- build.rs
+++ build.rs
@@ -1,6 +1,12 @@
 use std::process::Command;
 
 fn main() {
+    println!("cargo:rerun-if-changed=resources");
+
+    embed_resource::compile("resources/app.rc", embed_resource::NONE)
+        .manifest_required()
+        .expect("failed to embed resources");
+    
     // Try to get a short git revision; on failure, set to "unknown".
     let rev = Command::new("git")
         .args(["rev-parse", "--short", "HEAD"])
@@ -14,8 +20,7 @@ fn main() {
             }
         })
         .and_then(|v| String::from_utf8(v).ok())
-        .map(|s| s.trim().to_string())
-        .unwrap_or_else(|| "unknown".to_string());
+        .map_or_else(|| "unknown".to_string(), |s| s.trim().to_string());
 
-    println!("cargo:rustc-env=GIT_REVISION={}", rev);
+    println!("cargo:rustc-env=GIT_REVISION={rev}",);
 }
